5268680ba0b9639d0c06ac7fb45f7906fb85dc48
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-08-20 17:53:03 +0700

    dropdown

--- src/views/components/button/Dropdown.jsx
+++ src/views/components/button/Dropdown.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import IconWrap from '../div/icon'
+import { PhoneCallOut } from '../../../assets/icon/PhoneIcons'
+import { ChervonDown } from '../../../assets/icon/ActionIcons'
 
 const DropDown = ({ options }) => {
     return (
         <div className='input'>
             <div className="label"><span className='small-text'>Gọi từ dịch vụ</span></div>
             <div className="input-wrapper">
-                <input className='input-area' type="text" />
+                <IconWrap icon={PhoneCallOut} />
+                <input disabled className='input-area' type="text" value={'Dịch vụ mặc định'} />
+                <IconWrap icon={ChervonDown} />
             </div>
         </div>
     )
