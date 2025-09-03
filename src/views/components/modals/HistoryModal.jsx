5970931e54ef267026562e5b9ecad1a8a72dae5c
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-09-03 10:02:16 +0700

    modal header

---
+++ src/views/components/modals/HistoryModal.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { CloseIcon } from '../../../assets/icon/NewStyleIcon'
+import IconButton from '../button/IconButton'
+
+const HistoryModal = () => {
+    return (
+        <div style={{
+            position: 'absolute', top: '100%', right: '0',
+            display: "flex",
+            width: "320px",
+            flexDirection: "column",
+            alignItems: "flex-start",
+            borderRadius: '12px',
+            border: "1px solid var(--border-neutral-neutral-light, #DADCE5)",
+            background: "var(--background-neutral-surface, #F5F6FA)",
+            boxShadow: "0 8px 32px 0 rgba(0, 0, 0, 0.12)"
+        }}>
+            <div className="modal-header">
+                <div className="heading">
+                    <div className="primary-text">CS Call</div>
+                </div>
+                <div className="action-btn">
+                    <IconButton Icon={CloseIcon} size={20} />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default HistoryModal
--- src/views/drawer/CallHistory.jsx
+++ src/views/drawer/CallHistory.jsx
@@ -1,13 +1,14 @@
 import React, { useState } from 'react'
 import IconButton from '../components/button/IconButton'
 import { ClockClockwise } from '../../assets/icon/NewStyleIcon'
+import HistoryModal from '../components/modals/HistoryModal'
 
 const CallHistory = () => {
     const [showHistoryModal, setShowHistoryModal] = useState(false)
     return (
         <div style={{ position: 'absolute', top: '48px', right: '48px' }}>
             <IconButton Icon={ClockClockwise} />
-
+            <HistoryModal />
         </div>
     )
 }
