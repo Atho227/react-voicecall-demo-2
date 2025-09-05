5b810b3ff9eb00f08fe52b30ba0ad48efcda270d
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-09-05 15:49:43 +0700

    tạo thư mục app

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
-import MainLayout from './views/layouts/demo-page'
-import Controller from './views/components/other/controller'
-import LeftSideBar from './views/drawer/LeftSideBar'
-import { onReloaded } from './ultils/helper'
-import CallHistory from './views/drawer/CallHistory'
+import MainLayout from '../views/layouts/demo-page'
+import Controller from '../views/components/other/controller'
+import LeftSideBar from '../views/drawer/LeftSideBar'
+import { onReloaded } from '../ultils/helper'
+import CallHistory from '../views/drawer/CallHistory'
 
 function App() {
   useEffect(() => {
