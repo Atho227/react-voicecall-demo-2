7072d1148bce968eabf98e74cc126d544e2dbce9
Nguyen Thanh Long <longnt.22bi13264@usth.edu.vn>
2025-09-10 16:03:19 +0700

    .

---
+++ src/components/FormComponent/SidebarForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SidebarForm = ({ children, sx = {} }) => {
+    return (
+        <Box>
+            {children}
+        </Box>
+    )
+}
+
+export default SidebarForm
